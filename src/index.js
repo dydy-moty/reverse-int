ae5b256c0928be7035818e6807392df06a09586d
dydy-moty <ishmametev_m@mail.ru>
2022-01-04 11:08:58 +0500

    feat: add code in index file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    let str = n.toString();
+    if (n >= 0) {
+        return +str.split("").reverse().join("");
+    } else {
+        return +str.split("").slice(1).reverse().join("") ;
+    }
 }
+
